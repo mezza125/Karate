050afed8106a11f166ea7b3b206a08a6538b9dc3
Billy Brandon Meza Felix <brandon.mefe@gmail.com>
2025-06-18 15:45:42 -0500

    configuracion de bluid y ejecucion de test por consola

--- src/test/java/bdd/KarateRunner.java
+++ src/test/java/bdd/KarateRunner.java
@@ -1,10 +1,11 @@
 package bdd;
+
 import com.intuit.karate.junit5.Karate;
 
 public class KarateRunner {
 
     @Karate.Test
-   Karate testSample(){
+    Karate testSample() {
         return Karate.run("classpath:bdd/Features/Pruebas.feature");
     }
 
